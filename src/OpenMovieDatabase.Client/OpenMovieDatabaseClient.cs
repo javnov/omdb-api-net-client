113c37687491bac18191d0ffcd991a5933013ebe
Javier Novoa <javier.novoa@outlook.com>
2018-05-11 16:25:05 -0500

    Initial work

---
+++ src/OpenMovieDatabase.Client/OpenMovieDatabaseClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace OpenMovieDatabase.Client
+{
+    public class OpenMovieDatabaseClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public OpenMovieDatabaseClient(HttpClient httpClient, string apiKey)
+        {
+            if (string.IsNullOrEmpty(apiKey))
+                throw new ArgumentNullException(nameof(apiKey));
+
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _apiKey = apiKey;
+        }
+
+
+    }
+}
